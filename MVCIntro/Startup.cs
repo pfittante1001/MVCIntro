80a0ef1f2182dee4042dc67d5920204d01e39036
pfittante <pfittante@yahoo.com>
2017-07-24 10:45:57 -0400

    intitial commit

---
+++ MVCIntro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCIntro.Startup))]
+namespace MVCIntro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
